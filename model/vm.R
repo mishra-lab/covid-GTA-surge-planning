3bca5d6129513e2097d22dc55f06eb544f995aeb
David Landsman <david.landsman@mail.utoronto.ca>
2020-03-16 15:28:13 -0400

    Add covid model
    Connect input fields to model; read output and display `modelout` as a table

---
+++ model/vm.R
@@ -0,0 +1,69 @@
+#################################################
+# vm.R - View Model
+# - Exposes functions to be used in server login
+#################################################
+
+source('../model/epid.R')
+
+###FIXED############################################################################################
+# number of days, eg. 300 days. fix interval = 1
+times <-seq(0,300,1) 
+
+###FIXED################ external (e.g. imported cases as a time series) ###########################
+travel <-data.frame(times=times,import=rep(2,length(times))) #create an empty time-series of travel-related/imported cases
+travel$import <-ifelse(travel$times>15,3,2)  #fill in the time-series of travel-related/imported cases
+interp<-approxfun(travel,rule=2)  #create an interpolating function using approxfun
+####################################################################################################
+
+setupParams <- function (input) {
+	# duration of infectiousness
+	dur_inf = input$dur_subclinical + input$dur_symptomatic
+
+    # convert to transition rates
+	beta = input$R0 / dur_inf                      # transmission probability per capita
+	omega = 1 / input$dur_latent                   # latency rate from exposure to infectious per capita
+	alpha = 1 / input$dur_subclinical              # progression rate from subclinical to clinical per capita
+	g1 = 1 / input$dur_symptomatic                 # rate of recovery while symptomatic but not admitted
+	g2 = 1 / input$dur_admitted                    # rate of recover/discharge while admitted
+	g3 = 1 / input$dur_icu                         # rate of leaving ICU to medicine ward
+	rate_icu = (-log(1 - input$condprob_icu) * g2) / ((1 + log(1 - input$condprob_icu))) #I think this is correct formula using prob admit to ICU, not my previous one
+	death_rate = (-log(1 - input$condprob_cfr) * g3) /((1 + log(1 - input$condprob_cfr))) #I think this is the correct forumla using prob death among those in ICU, not my previous one
+	length_of_stay = (1 / (g2 + rate_icu)) + (1 / (g3 + death_rate))   #average length of stay in days in hospital (admission + icu) [probably need to add those who returned from ICU, but ok for now
+
+	##################################################################################
+	# set parameter table as a matrix or data frame
+	# the paramMat data.frame will be read in after being generated in another script
+	##################################################################################
+	paramMat = data.frame(
+		beta,                     		# transmission probability per capita
+		omega,                    		# latency rate from exposure to infectious per capita
+		alpha,                    		# progression rate from subclinical to clinical per capita
+		g1,                       		# rate of recovery while symptomatic but not admitted
+		g2,                       		# rate of recover/discharge while admitted
+		g3,                       		# rate of recovery/discharge while in icu
+		prob_test = input$prob_test,                # probability of testing+detection once clinical symptoms
+		prob_admit = input$prob_admit,               # probability of admission once symptomatic (assuming also tested)
+		rate_icu,                 		# rate of icu admission once admitted
+		death_rate,               		# disease-attributable mortality rate
+		drop_Reffective = input$drop_Reffective,          # factor by who much Reffective drops after outbreak takes off
+		R0 = input$R0,                       # included here for ease of adding to the outputs
+		seed_backCalc = input$seed_backCalc,            # initial seeding number
+		initpop = input$initpop,                  # popualtion size of Toronto
+		prop_travel_test = input$prop_travel_test,         # proportion travelers with COVID + symptoms who get tested/detected
+		social_distancing = input$social_distancing,        # time in days from seeding, when social distancing measures reduce beta by "drop_Reffective"
+		tau_1 = input$tau_1,                    # probability of testing patients [who do not need to be admitted] in the absence of travel history or epidemiological link
+		tau_2 = input$tau_2,                    # probability of testing inpatients for COVID in the absence of travel history or epidemiological link
+		tau_1_max = input$tau_1_max,                # increase rate to this probability of testing patients [who do not need to be admitted] in the absence of travel history or epidemiological link
+		tau_2_max = input$tau_2_max,                # increase rate to this probability of testing inpatients for COVID in the absence of travel history or epidemiological link. but probability will not be 100%
+		length_of_stay,           		# note, a calculated parameter that could also be calculated inside the model function
+		Ncases_trigger = input$Ncases_trigger,           # number of detected cases that triggers the rise in testing
+		event_ss = input$event_ss,                 # number of super-spreading events
+		event_ss_modulo = input$event_ss_modulo	        # frequency of super-spreading events
+	)
+
+	paramMat
+}
+
+runSimulation <- function (paramMat) {
+	epid(paramMat)
+}
