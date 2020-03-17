162a357e93ef72a28b0810b3c92ae88b3e0dabd2
David Landsman <david.landsman@mail.utoronto.ca>
2020-03-17 17:15:56 -0400

    Add script for running shiny app

---
+++ src/run_shiny.R
@@ -0,0 +1,8 @@
+library(shiny)
+
+options(
+    shiny.autoreload = TRUE, # TODO: figure out why this doesn't work...
+    shiny.autoreload.pattern = glob2rx("*.R"),
+    shiny.launch.browser = TRUE
+)
+runApp('app')
