4136bafaca7fdf0db522b8c06f33a5ad00fb75ec
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-01 16:36:07 -0400

    Switch to using rocker image
    Try adding secret for shinyapps deployment step

--- .github/actions/deployment/entrypoint.sh
+++ .github/actions/deployment/entrypoint.sh
@@ -2,16 +2,16 @@
 
 cd ./src/
 
-echo "Installing R..."
-apt-get update
-DEBIAN_FRONTEND=noninteractive apt-get install -y -q r-base r-base-core r-base-dev r-cran-littler
-apt-get install -y -q build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
+# echo "Installing R..."
+# apt-get update
+# DEBIAN_FRONTEND=noninteractive apt-get install -y -q r-base r-base-core r-base-dev r-cran-littler
+# apt-get install -y -q build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
 
 echo 'Installing R packages...'
 Rscript ./scripts/install_deps.R
 
-echo "Setting up shinyapps env..."
-# TODO: do we need this?
+# echo "Setting up shinyapps env..."
+
 
 echo "Running deploy_app.R..."
 Rscript ./scripts/deploy_app.R
--- src/scripts/deploy_app.R
+++ src/scripts/deploy_app.R
@@ -4,6 +4,14 @@ options(
     rsconnect.error.trace=TRUE
 )
 
+TOKEN <- Sys.getenv('SHINYAPPS_TOKEN')
+SECRET <- Sys.getenv('SHINYAPPS_SECRET')
+
+rsconnect::setAccountInfo(name='mishra-lab',
+    token=TOKEN,
+    secret=SECRET
+)
+
 rsconnect::deployApp(
     appDir='./app',
     # appFileManifest='./manifest',
