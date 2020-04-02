789a7112915e19c150bb8933f3165cb23eeb8360
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-02 11:37:49 -0400

    Add build skipping [ci skip]

--- .github/actions/deployment/entrypoint.sh
+++ .github/actions/deployment/entrypoint.sh
@@ -2,16 +2,8 @@
 
 cd ./src/
 
-# echo "Installing R..."
-# apt-get update
-# DEBIAN_FRONTEND=noninteractive apt-get install -y -q r-base r-base-core r-base-dev r-cran-littler
-# apt-get install -y -q build-essential libcurl4-gnutls-dev libxml2-dev libssl-dev
-
 echo 'Installing R packages...'
 Rscript ./scripts/install_deps.R
 
-# echo "Setting up shinyapps env..."
-
-
-echo "Running deploy_app.R..."
+echo "Running deployment script..."
 Rscript ./scripts/deploy_app.R
