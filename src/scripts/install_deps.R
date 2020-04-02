e1e45f385d63138d79d1b2f0c659bc6e859b9c0d
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-02 10:48:29 -0400

    Cleanup install script

--- src/scripts/install_deps.R
+++ src/scripts/install_deps.R
@@ -1,8 +1,5 @@
 options(repos=c(CRAN='http://cran.rstudio.com/'))
 
 install.packages('remotes')
-install.packages('packrat')
-
-# TODO: switch to renv, so we can pull installed packages from global lib
+remotes::install_github('rstudio/renv')
 renv::restore()
-remotes::install_github('rstudio/rsconnect')
