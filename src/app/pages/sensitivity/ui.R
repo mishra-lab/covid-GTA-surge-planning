752da8240f8664ef1b4442dbce2fd22442a389ea
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-06 16:40:13 -0400

    Add data + layout of sensitivity page

--- src/app/pages/sensitivity/ui.R
+++ src/app/pages/sensitivity/ui.R
@@ -1,10 +1,22 @@
 ui <- fluidPage(
-  	shiny::titlePanel('COVID-19 Healthcare Surge Model for Greater Toronto Area Hospitals'),
-  	
-	shiny::mainPanel(
-		shiny::wellPanel(
-			shiny::h3('Sensitivity Analysis', style='margin-top: 0;'),
-			shiny::uiOutput('parameterTabs')
+  	shiny::titlePanel('COVID-19 Healthcare Surge Model: Sensitivity Analysis'),
+	shiny::br(),
+
+	shiny::sidebarLayout(
+		shiny::sidebarPanel(
+			shiny::h3('Model Parameters', style='margin-top: 0;'),
+			shiny::selectInput(
+				'parameterSelect',
+				'Select a parameter for the sensitivity analysis',
+				c('prob_test')
+			)
+		),
+		
+		shiny::mainPanel(
+			shiny::wellPanel(
+				shiny::h3('Sensitivity Analysis Plot', style='margin-top: 0;'),
+				plotly::plotlyOutput('sensitivityPlot') %>% shinycssloaders::withSpinner(),
+			)
 		)
 	)
 )
