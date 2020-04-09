f5e765308b2ee4e2775eb5ef9d83584384c12f03
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-09 13:47:21 -0400

    Add info + code buttons

--- src/scripts/run_shiny.R
+++ src/scripts/run_shiny.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 options(
-    shiny.autoreload = TRUE, # TODO: figure out why this doesn't work...
+    shiny.autoreload = TRUE,
     shiny.autoreload.pattern = glob2rx("*.R"),
     shiny.launch.browser = TRUE
 )
