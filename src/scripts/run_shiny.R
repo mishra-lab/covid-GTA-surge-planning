7dfbf4a6227538518dc44f7a106b7705e1d4485a
David Landsman <david.landsman@mail.utoronto.ca>
2020-04-13 13:54:38 -0400

    Separate bed parameters from simulation
    Now, changing bed parameters doesn't cause model to re-run

--- src/scripts/run_shiny.R
+++ src/scripts/run_shiny.R
@@ -3,6 +3,7 @@ library(shiny)
 options(
     shiny.autoreload = TRUE,
     shiny.autoreload.pattern = glob2rx("*.R"),
-    shiny.launch.browser = TRUE
+    shiny.launch.browser = TRUE,
+    shiny.reactlog=TRUE
 )
 runApp('./app')
